b46b0590f1d27bfbac91e6c58254032c06e0a90a
David Wendland <dwndland@gmail.com>
2024-04-12 19:59:15 +0200

    Remove old demo projects

--- Demo2/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-// -----------------------------------------------------------------------------------------------------------------
-// <copyright file="AssemblyInfo.cs" company="my-libraries">
-//     Copyright (c) David Wendland. All rights reserved.
-// </copyright>
-// -----------------------------------------------------------------------------------------------------------------
-
-using System.Windows;
-
-[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
